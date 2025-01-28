c4a19c7f42b4b28661aaa9c2f3d222cf269c518c
Tepanyega <samuelmonepe@gmail.com>
2025-01-28 10:00:34 +0200

    Implementing the Caesar Cipher

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import Assignment2.CaesarCipher;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        String message = "FIRST LEGION ATTACK EAST FLANK!";
+        int key = 3;
+        System.out.println(CaesarCipher.encrypt(message, key));
+        System.out.println("\n" + "Testing by reading from file: "+ "\n");
+        CaesarCipher.testCaesar();
+        System.out.println("\n" + "Encrypt using two keys");
+        System.out.println(CaesarCipher.encryptTwoKeys(message, key, 4)); //please check the spaces when using the two keys;
     }
 }
